71bfaf866bc21d1e5bae74044715ada01e20f731
Erekle Magradze <emagradze@targamos.herkulanum.net>
2018-11-06 11:41:51 +0400

    swapargs

---
+++ Lect7/swapargs_pointers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void swapargs(int *x, int *y);
+
+int main()
+	{
+		int i, j;
+		i = 10;
+		j=19;
+		cout<<"mimdinare mnishvnelobebi"<<endl;;
+		cout<<"i: "<<i<<" , j: "<<j<<endl;
+		swapargs(&i,&j); 
+		cout<<"mnishvnelobebi adgilis shecvlis shemdeg"<<endl;
+		cout<<"i: "<<i<<" , j: "<<j<<endl;
+		return 0;
+	}
+
+void swapargs(int *x, int *y)
+	{
+		int t;
+		t = *x;
+		*x = *y;
+		*y=t;
+	}
