d7aae1a0560a970db62e667dbea32fd6855ce40c
Erekle Magradze <erekle.magradze@cern.ch>
2018-12-18 14:40:22 +0400

    array of pointers

---
+++ Lect11/arrayofpointers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+  {
+    int a[5]={1,2,3,4,5}, i;
+    int *p;
+    p = a;  //or p = &a[0];
+    for(i=0;i<5;i++){
+      cout<<*p<<endl;
+      p++;
+      }
+    return 0;
+  }
