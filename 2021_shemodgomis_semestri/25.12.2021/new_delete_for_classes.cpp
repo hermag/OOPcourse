370b26d54c501af831e57b13246eaf2e83e98a7b
Erekle Magradze <erekle@magradze.de>
2021-12-25 11:13:02 +0400

    adding more code

---
+++ 2021_shemodgomis_semestri/25.12.2021/new_delete_for_classes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class samp
+{
+    int i,j;
+public:
+    void set_ij(int a, int b) { i=a; j=b; }
+    int get_product() {return i*j; }
+};
+
+int main()
+{
+    samp *p;
+    p = new samp; //მეხსიერების გამოყოფა samp კლასის ობიექტისთვის
+    if (!p) 
+    {
+        cout<<"Cannot allocate memory"<<endl;
+        return 1;
+    }
+    p->set_ij(4,5);
+    cout<<"ეს არის მთელი რიცხვი რომელზეც მიუთითებს p: "<<p->get_product()<<endl;
+    delete p; //მეხსიერების განთავისუფლება
+    return 0;
+}
